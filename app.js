807660823fe3c7e78b9ca5097e871aef76d1f197
Graham McGregor <dev@grahammcgregor.ca>
2014-11-14 01:40:42 -0500

    Get input from user for hostname / ip

--- app.js
+++ app.js
@@ -3,22 +3,22 @@
 
   window.app = angular.module('jolokiaWebConsole', ['jsTree.directive']);
 
-  window.app.factory('JolokiaClient',
+  app.factory('JolokiaClient',
   	['$http',
     function ($http) {
       var _client = {},
           _baseUrl;
 
-      _client.setServer = function(server, port){
-        port = port || '8080';
+      _client.setServer = function(server){
         server = server || 'localhost';
 
-        var url = [server, ':', port, '/jolokia'];
-        $http.get(url).then(
+        var url = [server, ':', '8080', '/jolokia'];
+        var promise = $http.get(url).then(
           function (){
             _baseUrl = url;
           }
         );
+        return promise;
       };
 
       _client.list = function(path) {
@@ -35,9 +35,22 @@
     }]
   );
 
-  window.app.controller('HomeCtrl',
+  app.controller('HomeCtrl',
     ['$scope', 'JolokiaClient',
     function ($scope, JolokiaClient) {
+
+      function initRootTree(){
+        JolokiaClient.list('').then(
+          function (response){
+            //TODO need to process response
+            // response.data.value is the root list
+            // $scope.treeData = response.data.value;
+          },
+          function (){
+            // failed...
+          }
+        );
+      }
 
       $scope.treeTypesConfig = {
         "default": {
@@ -50,29 +63,26 @@
           "icon": "/images/file.png"
         }
       };
-      $scope.sampleData = [{
-        "id": "ajson1",
-        "parent": "#",
-        "text": "Simple root node"
-      }, {
-        "id": "ajson2",
-        "parent": "#",
-        "type": "folder",
-        "text": "Root node 2"
-      }, {
-        "id": "ajson3",
-        "parent": "ajson2",
-        "text": "Child 1"
-      }, {
-        "id": "ajson4",
-        "parent": "ajson2",
-        "text": "Child 2"
-      }];
+
+      $scope.treeData = [];
 
       // directive doesn't pick up initial data without this
       setTimeout( function(){
         $scope.$apply();
       }, 0);
+
+      $scope.setServer = function () {
+        // show loading symbol
+        JolokiaClient.setServer($scope.hostname).then(
+          function (response) {
+            initRootTree();
+          },
+          function () {
+            // failed...
+          }
+        );
+
+      };
     }
     ]);
 }());
